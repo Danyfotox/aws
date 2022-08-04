be2a03a7ccc3531ab9414d0241178840c1546460
danyfotox <lramosm1400@alumno.ipn.mx>
2022-08-04 15:14:14 -0500

    aws

---
+++ index.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+
+
+
+//settings
+app.set('port',process.env.PORT || 3000);
+
+//static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// start the server
+const server = app.listen(app.get('port'),()=> {
+    console.log('server on port: ', app.get('port'));
+});
+
+
+
+
+// websockets
+const SocketIO = require('socket.io');
+const io= SocketIO(server);
+
+
+app.get("/items",function(req,res){
+    var alarm = req.query.id;
+    res.send("hola: " + alarm);
+    io.sockets.emit('señal','peligro');
+});
+
+io.on('connection', (socket)=> {
+    console.log('new connection', socket.id);
+    
+    socket.on('chat:message',(data)=>{
+        io.sockets.emit('chat:message', data);
+    });
+
+    socket.on('chat:typing', (data) => {
+        socket.broadcast.emit('chat:typing', data);
+    });
+
+    socket.on('alarma',(data)=>{
+        io.sockets.emit('alarma',data);
+    });
+    socket.on('señal',(alarm)=>{
+        io.sockets.emit('señal',alarm);
+    });
+});
+
+
+// GET
---
+++ public/chat.js
@@ -0,0 +1,52 @@
+const socket = io()
+
+
+// string
+const data_to_pass_in = 'Send this to python script';
+
+// DOM elements
+let message = document.getElementById('message');
+let username = document.getElementById('username');
+let btn = document.getElementById('send');
+let output = document.getElementById('output');
+let actions = document.getElementById('actions');
+let output2 =document.getElementById('output2');
+let btn2 = document.getElementById('send2');
+
+btn.addEventListener('click', function(){
+    socket.emit('chat:message',{
+        message: message.value,
+        username: username.value
+    });
+});
+
+btn2.addEventListener('click', function(){
+    socket.emit('alarma',{
+        message: "hola"
+    });
+})
+
+message.addEventListener('keypress', function(){
+    console.log(username.value);
+    socket.emit('chat:typing', username.value);
+});
+
+socket.on('chat:message',function(data){
+    actions.innerHTML = '';
+    output.innerHTML +=`<p>
+    <strong>${data.username}</strong>: ${data.message}
+    </p>`
+});
+
+socket.on('chat:typing', function(data){
+    actions.innerHTML = `<p><em>${data} is typing a message.</em></p>`
+});
+
+socket.on('alarma',function(data){
+    output2.innerHTML += `<p>${data.message} </p>`
+});
+
+socket.on('señal',function(data){
+    output2.innerHTML += `<p>${data} </p>`
+});
+
