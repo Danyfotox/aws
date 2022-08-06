f1f962f8b9506d723ac6085d81dd2e4851b2e7dc
danyfotox <lramosm1400@alumno.ipn.mx>
2022-08-06 14:03:41 -0500

    port8080

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 
 
 //settings
-app.set('port',process.env.PORT || 3000);
+app.set('port',process.env.PORT || 8080);
 
 //static files
 app.use(express.static(path.join(__dirname, 'public')));
